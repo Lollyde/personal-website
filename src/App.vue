afee6a1d28accafc718aad5c9886660d98ed6a36
LoLlYdE <11347524+LoLlYdE@users.noreply.github.com>
2019-08-06 18:26:14 +0200

    use reusable content component

--- src/App.vue
+++ src/App.vue
@@ -9,22 +9,61 @@
 <script>
 import Header from "./components/parts/Header";
 import Flag from "./components/parts/Flag";
-import Home from "./components/pages/Home";
-import Portfolio from "./components/pages/Portfolio";
-import Contact from "./components/pages/Contact";
-import About from "./components/pages/About";
-import NotFound from "./components/pages/NotFound";
+import Content from "./components/parts/Content";
 import VueRouter from 'vue-router';
+
+const component_data = {
+  home_data: {
+    header: `Hello.`,
+    content: [
+      `I'm Lily. I'm a developer currently living in germany.`,
+      `I mainly focus on desktop applications, though I'm learning webdev and am willing to learn pretty much anything.`,
+      `I am also an avid Twitch viewer and moderator.`,
+      `Welcome to my site!`
+    ]
+  },
+  portfolio_data: {
+    header: `Portfolio`,
+    content: [
+      `Most of my projects can be found on <a href="https://github.com/lollyde">github</a>.`,
+      `I will be adding more as I finish projects.`
+    ]
+  },
+  contact_data: {
+    header: `Contact`,
+    content: [
+      `I can be reached via a number of ways:`,
+      `Email: <a href="mailto:Lily@lollyde.online">Lily@lollyde.online</a>`,
+      `Twitter: <a href="https://twitter.com/lollyde_">lollyde_</a>`
+    ]
+  },
+  about_data: {
+    header: `About`,
+    content: [
+      `This site was built using <a href="https://vuejs.org/">Vue.js</a>`,
+      `Icons made by <a href="https://3v.fi">3v</a>`,
+      `Background made using a slightly modified version of <a href="https://vincentgarreau.com/particles.js/">particles.js</a>`,
+      `This website is hosted on my own server with <a href="https://nginx.org/">Nginx</a>`
+    ]
+  },
+  notfound_data: {
+    header: `404`,
+    content: [
+      `Requested page wasnt found :(`
+    ]
+  }
+
+};
 
 const router = new VueRouter({
   mode: 'history',
   base: __dirname,
   routes: [
-    { path: '/', component: Home },
-    { path: '/portfolio', component: Portfolio },
-    { path: '/contact', component: Contact },
-    { path: '/about', component: About },
-    { path: '*', component: NotFound }
+    { path: '/', component: Content, props: () => ({ content: component_data.home_data.content, header: component_data.home_data.header }) },
+    { path: '/portfolio', component: Content, props: () => ({ content: component_data.portfolio_data.content, header: component_data.portfolio_data.header }) },
+    { path: '/contact', component: Content, props: () => ({ content: component_data.contact_data.content, header: component_data.contact_data.header }) },
+    { path: '/about', component: Content, props: () => ({ content: component_data.about_data.content, header: component_data.about_data.header }) },
+    { path: '*', component: Content, props: () => ({ content: component_data.notfound_data.content, header: component_data.notfound_data.header }) }
   ]
 })
 
---
+++ src/components/parts/Content.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="content_wrapper">
+    <div class="content">
+      <h1>{{header}}</h1>
+      <div class="description">
+        <p v-for="line in content" v-bind:key="line" v-html="line"></p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Content",
+  components: {},
+  props: ["header", "content"]
+};
+</script>
+
+<style>
+.content_wrapper {
+  left: 50%;
+  position: absolute;
+  pointer-events: none;
+  width: 50%;
+  margin-top: 72px;
+}
+.content {
+  background-color: rgba(0, 0, 0, 0);
+  z-index: 10;
+  text-align: center;
+  padding: 5px;
+  position: relative;
+  left: -50%;
+}
+.description {
+  font-size: 120%;
+  color: lightgray;
+}
+</style>
