052dcd3b13826e0eb83463ab649f2ff5edc8d11f
LoLlYdE <11347524+LoLlYdE@users.noreply.github.com>
2019-11-10 22:10:48 +0100

    add lots of stuff to portfolio

--- src/App.vue
+++ src/App.vue
@@ -25,8 +25,23 @@ const component_data = {
   portfolio_data: {
     header: `Portfolio`,
     content: [
-      `Most of my projects can be found on <a href="https://github.com/lollyde">github</a>.`,
-      `I will be adding more as I finish projects.`
+      `<h2>Languages</h2>`,
+      `Learning Java since 2014, actively developing for three years`,
+      `Learning C# since 2016, actively developing since then`,
+      `A bit of JS I picked up over the years`,
+      `Rudimentary experience in Lisp (Scheme dialect), Python, Node.js, Bash`,
+      `Currently also learning Go`,
+      `<br>`,
+      `<h2>Projects</h2>`,
+      `Contributor to the <a href="https://github.com/EverestAPI/Everest">EverestAPI</a> modloader for Celeste`,
+      `<a href="https://github.com/lollyde/CelesteRandomizer">Celeste Randomizer</a>, a mod for Celeste using the EverestAPI`,
+      `A couple of simple Twitch chatbots implemented using <a href="https://github.com/TwitchLib/TwitchLib">TwitchLib</a>`,
+      `<a href="https://github.com/lollyde/personal-website">This website</a>, using Vue.js`,
+      `"Nine men's morris" AI written in Java (Uni project)`,
+      `"Organization Helper" app for Android + backend developed in Java (Uni project)`,
+      `Sadly I cannot provide the source code for my Uni projects.`,
+      `Other projects can be found on <a href="https://github.com/lollyde">github</a>`,
+      `I will be adding more as I finish projects`
     ]
   },
   contact_data: {
